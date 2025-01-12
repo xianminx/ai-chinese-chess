638eab243a8b2687d0cf85f9b11abc8cc1052945
Lucas <xianminx@gmail.com>
2025-01-12 14:07:14 +0800

    feat: add new icons and update app metadata for Chinese Chess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Game from "@/components/Game";
 import Footer from "@/components/Footer";
-
+import Image from "next/image";
 export default function Home() {
     return (
         <div className="grid grid-rows-[1fr_auto] items-center justify-items-center min-h-screen pb-8 gap-8 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 items-center sm:items-start">
                 <Game />
+                <Image src="/icon.svg" alt="icon" width={50} height={50} />
             </main>
             <Footer />
         </div>
