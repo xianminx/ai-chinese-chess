8bbdb63ac49511993a642d32a79d1bcb90da3d78
Lucas <xianminx@gmail.com>
2025-01-13 20:26:12 +0800

    refactor: update prompt version management

--- src/app/api/move/prompts.ts
+++ src/app/api/move/prompts.ts
@@ -1,6 +1,7 @@
 import { DIFFICULTY_PROMPTS as DIFFICULTY_PROMPTS_2, SYSTEM_PROMPT as SYSTEM_PROMPT_2, getUserPrompt as getUserPrompt_2 } from "./prompts_2";
 import { DIFFICULTY_PROMPTS as DIFFICULTY_PROMPTS_1, SYSTEM_PROMPT as SYSTEM_PROMPT_1, getUserPrompt as  getUserPrompt_1 } from "./prompts_1";
 
+// eslint-disable-next-line prefer-const
 let PROMPT_VERSION = 2;
 
 export const SYSTEM_PROMPT = PROMPT_VERSION === 1 ? SYSTEM_PROMPT_1 : SYSTEM_PROMPT_2;
